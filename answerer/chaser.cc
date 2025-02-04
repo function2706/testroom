62089720b359ea9981e8df79a7d5c5ffaa24dd43
Yuta Iwasaki <yuta.iwasaki@silex.jp>
2025-02-04 17:27:44 +0900

    Improve syntax.

--- answerer/chaser.cc
+++ answerer/chaser.cc
@@ -13,11 +13,19 @@
 /* =================================== 擬ライブラリ =================================== */
 
 #include <unistd.h>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
 #include <vector>
+
+#define debuglog(fmt, arg...)                                            \
+	do {                                                             \
+		printf("%s(L.%d) " fmt "\n", __FILE__, __LINE__, ##arg); \
+	} while (0)
+
+#define xxxxxMARKERxxxxx debuglog("through")
 
 /**
  * @brief 標準入力の文字列を扱うクラス
@@ -49,11 +57,11 @@ void fstring::fget()
  * @brief 以下のフォーマットに則した文字列から生成される整数配列クラス
  * "[整数] [整数] ... [整数]"
  */
-class intvector : public std::vector<int>
+class vint32 : public std::vector<int32_t>
 {
 public:
-	intvector() { fget(); }
-	intvector(const char* str) { get(str); }
+	vint32() { fget(); }
+	vint32(const char* str) { get(str); }
 
 	void add(const char* str);
 	void get(const char* str);
@@ -68,7 +76,7 @@ public:
  *
  * @param str フォーマットに即した文字列
  */
-void intvector::add(const char* str)
+void vint32::add(const char* str)
 {
 	char dstr[4096] = {0}, *term, *head;
 	size_t restlen = strlen(str);
@@ -100,7 +108,7 @@ void intvector::add(const char* str)
  *
  * @param str フォーマットに即した文字列
  */
-void intvector::get(const char* str)
+void vint32::get(const char* str)
 {
 	this->clear();
 	add(str);
@@ -113,7 +121,7 @@ void intvector::get(const char* str)
  *
  * @exception fgets 失敗時に -1
  */
-void intvector::fadd()
+void vint32::fadd()
 {
 	char str[4096] = {0};
 
@@ -131,7 +139,7 @@ void intvector::fadd()
  *
  * @exception fgets 失敗時に -1
  */
-void intvector::fget()
+void vint32::fget()
 {
 	this->clear();
 	fadd();
@@ -148,14 +156,24 @@ enum class objid : char
 	wall = '#',
 };
 
+using dirbit = uint8_t;
+namespace dir
+{
+	static uint8_t none = 0x0;
+	static uint8_t east = 0x1;
+	static uint8_t nowth = 0x2;
+	static uint8_t west = 0x4;
+	static uint8_t south = 0x8;
+}  // namespace dir
+
 class map
 {
-	int w_;
-	int h_;
-	char* m_;
+	uint32_t w_;
+	uint32_t h_;
+	objid* m_;
 
-	int me_x_;
-	int me_y_;
+	uint32_t me_x_;
+	uint32_t me_y_;
 
 public:
 	map();
@@ -164,36 +182,37 @@ public:
 
 	void operator=(const map& m);
 
-	int width() const { return w_; }
-	int height() const { return h_; }
-	int me_x() const { return me_x_; }
-	int me_y() const { return me_y_; }
+	uint32_t width() const { return w_; }
+	uint32_t height() const { return h_; }
+	uint32_t me_x() const { return me_x_; }
+	uint32_t me_y() const { return me_y_; }
 
-	char get(int x, int y) const;
-	void set(char id, int x, int y);
-	void set(objid id, int x, int y) { set((char)id, x, y); }
+	objid get(uint32_t x, uint32_t y) const;
+	void set(objid id, uint32_t x, uint32_t y);
 
-	bool is_match(objid id, int x, int y) const { return get(x, y) == (char)id; }
+	bool is_match(objid id, uint32_t x, uint32_t y) const { return get(x, y) == id; }
 
-	int moveme_with(int dx, int dy);
+	dirbit get_accesible_dir() const;
+	bool is_accesible_to(dirbit dir) const;
+	int32_t moveme_with(dirbit dir);
 
 	void print() const;
 };
 
 map::map() : w_(0), h_(0), m_(nullptr), me_x_(0), me_y_(0)
 {
-	intvector iv;
+	vint32 iv;
 
 	h_ = iv[0];
 	w_ = iv[1];
 
-	m_ = new char[w_ * h_];
+	m_ = new objid[w_ * h_];
 
-	for (int y = 0; y < h_; y++) {
+	for (uint32_t y = 0; y < h_; y++) {
 		fstring fstr;
 
-		for (int x = 0; x < w_; x++) {
-			set(fstr[x], x, y);
+		for (uint32_t x = 0; x < w_; x++) {
+			set((objid)fstr[x], x, y);
 			if (fstr[x] == 'A') {
 				me_x_ = x;
 				me_y_ = y;
@@ -202,10 +221,9 @@ map::map() : w_(0), h_(0), m_(nullptr), me_x_(0), me_y_(0)
 	}
 }
 
-map::map(const map& m) : w_(m.width()), h_(m.height()), me_x_(m.me_x()), me_y_(m.me_y())
+map::map(const map& m)
 {
-	m_ = new char[w_ * h_];
-	memcpy(m_, m.m_, w_ * h_ * sizeof(char));
+	*this = m;
 }
 
 void map::operator=(const map& m)
@@ -214,28 +232,106 @@ void map::operator=(const map& m)
 	h_ = m.height();
 	me_x_ = m.me_x();
 	me_y_ = m.me_y();
-	m_ = new char[w_ * h_];
-	memcpy(m_, m.m_, w_ * h_ * sizeof(char));
+	m_ = new objid[w_ * h_];
+	memcpy(m_, m.m_, w_ * h_ * sizeof(objid));
 }
 
-char map::get(int x, int y) const
+/**
+ * @brief 指定の座標の値(objid)を取得
+ *
+ * @param x
+ * @param y
+ * @return objid 成功時に objid
+ *
+ * @exception 範囲不適時に例外 -1
+ */
+objid map::get(uint32_t x, uint32_t y) const
 {
+	if ((x > w_ - 1) || (y > h_ - 1)) {
+		throw -1;
+	}
 	return m_[y * w_ + x];
 }
 
-void map::set(char id, int x, int y)
+/**
+ * @brief 指定の座標に指定の値(objid)を代入
+ *
+ * @param id
+ * @param x
+ * @param y
+ *
+ * @exception 範囲不適時に例外 -1
+ */
+void map::set(objid id, uint32_t x, uint32_t y)
 {
+	if ((x > w_ - 1) || (y > h_ - 1)) {
+		throw -1;
+	}
 	m_[y * w_ + x] = id;
 }
 
 void map::print() const
 {
-	for (int y = 0; y < h_; y++) {
-		for (int x = 0; x < w_; x++) {
-			printf("%c", get(x, y));
+	for (uint32_t y = 0; y < h_; y++) {
+		for (uint32_t x = 0; x < w_; x++) {
+			printf("%c", (char)get(x, y));
 		}
 		printf("\n");
 	}
+}
+
+/**
+ * @brief me の現在地点から移動可能な方角を取得する
+ */
+dirbit map::get_accesible_dir() const
+{
+	dirbit resdir = dir::none;
+
+	try {
+		if (is_match(objid::path, me_x_ + 1, me_y_) ||
+		    is_match(objid::target, me_x_ + 1, me_y_)) {
+			resdir |= dir::east;
+		}
+	} catch (...) {
+	}
+
+	try {
+		if (is_match(objid::path, me_x_, me_y_ - 1) ||
+		    is_match(objid::target, me_x_, me_y_ - 1)) {
+			resdir |= dir::nowth;
+		}
+	} catch (...) {
+	}
+
+	try {
+		if (is_match(objid::path, me_x_ - 1, me_y_) ||
+		    is_match(objid::target, me_x_ - 1, me_y_)) {
+			resdir |= dir::west;
+		}
+	} catch (...) {
+	}
+
+	try {
+		if (is_match(objid::path, me_x_, me_y_ + 1) ||
+		    is_match(objid::target, me_x_, me_y_ + 1)) {
+			resdir |= dir::south;
+		}
+	} catch (...) {
+	}
+
+	return resdir;
+}
+
+/**
+ * @brief me は dir 方向へ移動可能か
+ *
+ * @param dir
+ * @return true
+ * @return false
+ */
+bool map::is_accesible_to(dirbit dir) const
+{
+	return (get_accesible_dir() & dir) != dir::none;
 }
 
 /**
@@ -246,23 +342,36 @@ void map::print() const
  * @param dy
  * @return int 移動回数, 移動不可時に -1
  */
-int map::moveme_with(int dx, int dy)
+int32_t map::moveme_with(dirbit dir)
 {
-	int result_x = me_x_ + dx, result_y = me_y_ + dy;
-
-	if ((result_x < 0) || (result_x > w_ - 1) || (result_y < 0) || (result_y > h_ - 1)) {
+	if (!is_accesible_to(dir)) {
 		return -1;
 	}
-	else if (is_match(objid::visited, result_x, result_y) ||
-		 is_match(objid::wall, result_x, result_y)) {
-		return -1;
+
+	int32_t result_x = me_x_, result_y = me_y_, ret = 0;
+
+	if (dir & dir::east) {
+		result_x++;
+		ret++;
+	}
+	if (dir & dir::nowth) {
+		result_y--;
+		ret++;
+	}
+	if (dir & dir::west) {
+		result_x--;
+		ret++;
+	}
+	if (dir & dir::south) {
+		result_y++;
+		ret++;
 	}
 
 	set(objid::visited, me_x_, me_y_);
 	set(objid::me, result_x, result_y);
 	me_x_ = result_x;
 	me_y_ = result_y;
-	return dx + dy;
+	return ret;
 }
 
 class chaser_meat
@@ -270,10 +379,10 @@ class chaser_meat
 	map origin_map_;
 
 	map marked_map_;
-	int moved_;
-	int min_moved_;
+	int32_t moved_;
+	int32_t min_moved_;
 
-	void min_move_to(int goal_x, int goal_y);
+	void min_move_to(uint32_t goal_x, uint32_t goal_y);
 
 public:
 	chaser_meat() : origin_map_(), marked_map_(origin_map_), moved_(0), min_moved_(-1) {}
@@ -282,7 +391,7 @@ public:
 	void answer();
 };
 
-void chaser_meat::min_move_to(int goal_x, int goal_y)
+void chaser_meat::min_move_to(uint32_t goal_x, uint32_t goal_y)
 {
 	sleep(1);
 	printf("moved=%d, min=%d\n", moved_, min_moved_);
@@ -302,7 +411,7 @@ void chaser_meat::min_move_to(int goal_x, int goal_y)
 		return;
 	}
 
-	if (marked_map_.moveme_with(1, 0) < 0) {
+	if (marked_map_.moveme_with(dir::east) < 0) {
 		// return;
 	}
 	else {
@@ -310,7 +419,7 @@ void chaser_meat::min_move_to(int goal_x, int goal_y)
 		min_move_to(goal_x, goal_y);
 	}
 
-	if (marked_map_.moveme_with(-1, 0) < 0) {
+	if (marked_map_.moveme_with(dir::nowth) < 0) {
 		// return;
 	}
 	else {
@@ -318,7 +427,7 @@ void chaser_meat::min_move_to(int goal_x, int goal_y)
 		min_move_to(goal_x, goal_y);
 	}
 
-	if (marked_map_.moveme_with(0, 1) < 0) {
+	if (marked_map_.moveme_with(dir::west) < 0) {
 		// return;
 	}
 	else {
@@ -326,7 +435,7 @@ void chaser_meat::min_move_to(int goal_x, int goal_y)
 		min_move_to(goal_x, goal_y);
 	}
 
-	if (marked_map_.moveme_with(0, -1) < 0) {
+	if (marked_map_.moveme_with(dir::south) < 0) {
 		// return;
 	}
 	else {
@@ -337,6 +446,7 @@ void chaser_meat::min_move_to(int goal_x, int goal_y)
 
 void chaser_meat::answer()
 {
+	origin_map_.print();
 	min_move_to(4, 2);
 	printf("%d\n", min_moved_);
 }
